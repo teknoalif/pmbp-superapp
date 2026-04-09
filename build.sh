141dfd3a60780a811ec9ab3c825e9ac1ce5bbebf
azharfa99 <azharfaturohman29@gmail.com>
2026-04-09 13:39:09 +0700

    Fix output directory and vercel config

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-# Install dependencies
+# Install library yang dibutuhkan
 pip install -r requirements.txt
 
-# Jalankan migrasi database
+# Jalankan migrasi database ke Supabase secara otomatis
 python manage.py migrate --noinput
 
-# Kumpulkan file statis (CSS/JS)
+# Kumpulkan file CSS dan JS agar tampilan rapi
 python manage.py collectstatic --noinput
