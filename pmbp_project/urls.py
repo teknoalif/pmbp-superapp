ac0240ffa1550e101f99efd6514f78917cf91aa4
azharfa99 <azharfaturohman29@gmail.com>
2026-04-11 06:07:41 +0700

    Final attempt: Clean routes and production settings

--- pmbp_project/urls.py
+++ pmbp_project/urls.py
@@ -1,15 +1,15 @@
-﻿from django.urls import path
-from core import views
-from django.contrib.auth import views as auth_views
+from django.contrib import admin
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from core import views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', views.logout_view, name='logout'), # Menggunakan view kustom
-    path('', views.input_laporan, name='input_laporan'),
-    path('dashboard/', views.dashboard_admin, name='dashboard_admin'),
-    path('registrasi-pembina/', views.tambah_pembina_dan_ekskul, name='tambah_pembina_ekskul'),
-    path('edit/<int:pk>/', views.edit_laporan, name='edit_laporan'),
-    path('hapus/<int:pk>/', views.hapus_laporan, name='hapus_laporan'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('admin/', admin.site.urls),
+    path('', views.dashboard_admin, name='dashboard_admin'),
+    path('input/', views.input_laporan, name='input_laporan'),
+    path('accounts/', include('django.contrib.auth.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
