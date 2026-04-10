a2a4bf1fcc3d3bb3f19e79bbb55c52f042b618e1
azharfa99 <azharfaturohman29@gmail.com>
2026-04-11 06:20:47 +0700

    Fix: Template paths and login redirect for production

--- pmbp_project/urls.py
+++ pmbp_project/urls.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 from core import views
 
 urlpatterns = [
@@ -10,6 +8,3 @@ urlpatterns = [
     path('input/', views.input_laporan, name='input_laporan'),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
